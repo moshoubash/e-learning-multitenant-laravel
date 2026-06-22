c31dfe9c90301900588144584836715424600041
moshoubash <ma016188@gmail.com>
2026-06-22 14:52:20 +0300

    feat: add description meta tags for all layouts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="GRID LMS - Learning Management System for Schools, Universities and Organizations">
 
     <x-ui.cdn-assets />
 
