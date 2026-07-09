c87a9eb6d2a3e3560481a2bf6b632d0d7539f6d0
moshoubash <ma016188@gmail.com>
2026-07-09 10:08:13 +0300

    style: update the modals width for mobile

--- resources/views/livewire/admin/users-components/modals/show-edit-modal.blade.php
+++ resources/views/livewire/admin/users-components/modals/show-edit-modal.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
             <div class="fixed inset-0 transition-opacity bg-on-surface/60" wire:click="closeModal"></div>
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-            <div class="inline-block overflow-hidden align-bottom transition-all transform ltr:text-left rtl:text-right bg-surface-container-lowest neo-border neo-radius sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div class="inline-block w-full overflow-hidden align-bottom transition-all transform ltr:text-left rtl:text-right bg-surface-container-lowest neo-border neo-radius sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                 <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <h3 class="mb-4 text-sm font-bold tracking-widest uppercase text-on-surface">{{ __('messages.Edit User') }}: {{ $editingUser->name }}</h3>
                     <form>
