ca79ebbe7aa2a75473f841b2966ae116bd8a9f90
moshoubash <ma016188@gmail.com>
2026-07-14 05:52:49 +0300

    feat: add Google Tag Manager integration to guest and public layouts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,15 @@
 
             gtag('config', 'G-SNMKLPJSCM');
         </script>
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-53S63VJQ');
+        </script>
+        <!-- End Google Tag Manager -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -36,6 +45,11 @@
         </style>
     </head>
     <body class="flex items-center justify-center min-h-screen p-4 font-sans antialiased">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53S63VJQ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
 
         @php $logo = app(\App\Services\DesignConfigService::class)->getLogo(); @endphp
         <main class="w-full max-w-md">
